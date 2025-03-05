8f7e090d29c5788287a1228f4a680b65cef6f591
blaine <blaine@qq.com>
2025-03-05 17:29:43 +0800

    add 前台站-新增getLangValueByField,根据字段名获取当前语言对应的值

--- frontend/app/common.php
+++ frontend/app/common.php
@@ -1,46 +1,63 @@
 <?php
 // 应用公共文件
 use think\model\Collection;
+use think\Model;
 
 /**
- * 根据语言后缀获取多语言的标题
- * @param Collection|array $mod 数据模型
- * @param string $langSuffix 语言后缀,值有
- * - en,英文
- * - cn,简体中文
- * - tw,繁体中文
- * - ja,日文
- * - ko,韩文
- * - ms,马来文
- * - th,泰文
- * - de,德文
- * - vi,越南文
- * - id,印尼文
- * - pt,葡萄牙文
- * - tlph,菲律宾文
+ * 获取当前语言标识
  * @return string
  */
-function getTitleByLang($mod, string $langSuffix = ''): string
+function getCurrentLangTag(): string
+{
+    $res = 'en';
+    //TODO 获取具体的语言
+
+//* - en,英文
+//* - cn,简体中文
+//* - tw,繁体中文
+//* - ja,日文
+//* - ko,韩文
+//* - ms,马来文
+//* - th,泰文
+//* - de,德文
+//* - vi,越南文
+//* - id,印尼文
+//* - pt,葡萄牙文
+//* - tlph,菲律宾文
+
+    return $res;
+}
+
+
+/**
+ * 根据字段名获取当前语言对应的值
+ * @param Model|array $mod 数据模型或数组
+ * @param string $fieldPrefix 字段名前缀(不含语言标识),默认为title；还有合作伙伴的hint、description。
+ * @return string
+ */
+function getLangValueByField($mod, string $fieldPrefix = 'title'): string
 {
 
-    $chk1 = $mod instanceof Collection;
+    $chk1 = $mod instanceof Model;
     $chk2 = is_array($mod);
-    if ($chk1 && $chk2) {
-        return '';
+    if (!$chk1 && !$chk2) {
+        return 'error';
     }
 
-    $default = $chk1 ? ($mod->title_en ?? '') : ($mod['title_en'] ?? '');
-    $default = trim($default);
+    $tag = getCurrentLangTag();
+    $fieldName = "{$fieldPrefix}_$tag";
+    $defaultField = "{$fieldPrefix}_en";
 
-    if (empty($langSuffix)) {
-        return $default;
-    }
+    $defaultValue = $chk1 ? ($mod->$defaultField ?? '') : ($mod[$defaultField] ?? '');
+    $defaultValue = trim($defaultValue);
 
-    $field = "title_{$langSuffix}";
-    $res = $chk1 ? ($mod->$field ?? '') : ($mod[$field] ?? '');
-    $res = trim($res);
-    if (empty($res)) {
-        $res = $default;
+    $fieldValue = $chk1 ? ($mod->$fieldName ?? '') : ($mod[$fieldName] ?? '');
+    $fieldValue = trim($fieldValue);
+
+    if (empty($fieldValue)) {
+        $res = $defaultValue;
+    } else {
+        $res = $fieldValue;
     }
 
     return $res;
